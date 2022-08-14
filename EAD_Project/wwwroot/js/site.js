3627f62cc26769de3be9156a897c3e9daf8f80b2
Muhammad Hassan <hmushtaq720@gmail.com>
2022-08-14 13:49:09 -0700

    Add functionality to hide and show menu on mobile version

--- EAD_Project/wwwroot/js/site.js
+++ EAD_Project/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function(){
+    $("#menuButton").click(function(){
+        if(document.getElementById("menuContents").style.visibility == "hidden"){
+            document.getElementById("menuContents").style.visibility = "visible"
+        }
+        else{
+            document.getElementById("menuContents").style.visibility = "hidden"
+        }
+    });
+});
